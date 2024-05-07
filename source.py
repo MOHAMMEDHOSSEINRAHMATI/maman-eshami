d14508af06cd155d4f6e6b89744baf03fc0c0c80
محمدحسین حسین رحمتی <157976834+MOHAMMEDHOSSEINRAHMATI@users.noreply.github.com>
2024-05-07 14:24:52 +0330

    Update source.py
    Ehshami hot momy!🥰

--- source.py
+++ source.py
@@ -1,1 +1,20 @@
+from colorama import Fore, Style
+import random
+import os
 
+# ehshami_mom_project
+# code_by_MOhAMMED_HOSSEIN_RAHMATI
+os.system('pip install colorama')
+momy = input(f"{Fore.GREEN}maman ehshami chie?:")
+
+if momy == "jende" and momy == "kostopoli":
+     print("ok your EQ 100 <3")
+else:
+    while momy != "jende" and momy != "kos topoli":
+        mom = input(f"{Fore.GREEN}maman ehshami chie?:")
+        if mom == "jende" and mom == "kostopoli":
+            print("nice <3")
+        else:
+            for kos in range(0, 101):
+                darsad = random.randint(1, 100)
+                print("bayad goft maman eshami", "%", darsad, "jende ast")
